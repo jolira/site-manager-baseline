187f3d8a18269584d81dfa5f7cf0357101771055
Joachim Kainz <jfk@jolira.com>
2012-07-11 16:18:04 -0700

    now generating uuids using app.uuid()

--- public/js/app-starter.js
+++ public/js/app-starter.js
@@ -51,4 +51,13 @@
 
         init();
     });
+
+    // UUID Generator from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+    function S4() {
+        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    }
+
+    app.uuid = function() {
+        return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+    }
 })($, _, Backbone, window["jolira-app"]);
